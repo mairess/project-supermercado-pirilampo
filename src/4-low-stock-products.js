4fd81f545963c57c1d3743edac2054f67c684451
Maires Souza <trabalho.maires@gmail.com>
2023-06-04 17:10:02 -0300

    feat: add getLowStockProducts function

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  const results = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock > 0 && stockProducts[index].quantityInStock <= 10) {
+      let name = stockProducts[index].productName;
+      let quantityProduct = stockProducts[index].quantityInStock;
+
+      results.push(`${name}: ${quantityProduct} unidades`);
+    }
+  }
+  return results;
 };
+
+getLowStockProducts();
 
 module.exports = { getLowStockProducts };
