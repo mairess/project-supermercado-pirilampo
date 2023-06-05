b65c1fcabe369dab7a20a846a3f34711fdc8e1d3
Maires Souza <trabalho.maires@gmail.com>
2023-06-05 20:40:38 -0300

    feat: add allergyOrIntoleranceMessage function

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,22 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
+  let newInfos = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].allergyOrIntolerance) {
+      let description = stockProducts[index].description;
+      let price = stockProducts[index].price;
+      let formattedPrice = `R$ ${price}`;
+      let allergyArray = stockProducts[index].allergyOrIntolerance;
+      let allergyArrayConvert = allergyArray.toString().replaceAll(',', ' ');
+      let message = 'Pode conter: ';
+      let allergyOrIntoleranceMessage = message + allergyArrayConvert;
+      newInfos.push({ description, formattedPrice, allergyOrIntoleranceMessage });
+    }
+  }
+  return newInfos;
 };
+
+getProductsWithAllergyOrIntoleranceInfo();
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
