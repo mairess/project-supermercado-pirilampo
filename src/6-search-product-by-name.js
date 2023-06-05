90c462552c2f343e1d05ae4a8547fd8567998130
Maires Souza <trabalho.maires@gmail.com>
2023-06-05 10:30:04 -0300

    feat: add searchProductByName function

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,23 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
-  // Desenvolva seu código dentro dessa função...
+let newInfos = null;
+const searchProductByName = (product) => {
+  if (!product) {
+    return null;
+  }
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (product === stockProducts[index].productName) {
+      let description = stockProducts[index].description;
+      let price = stockProducts[index].price;
+      let formattedPrice = `R$ ${price}`;
+      newInfos = { description, formattedPrice };
+      return newInfos;
+    }
+  }
+
+  return null;
 };
+
+searchProductByName();
 
 module.exports = { searchProductByName };
