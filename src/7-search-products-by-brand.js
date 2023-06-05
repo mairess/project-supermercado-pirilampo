989c519c4a9042e3e382594bde41ecfc56e1f59d
Maires Souza <trabalho.maires@gmail.com>
2023-06-05 16:05:08 -0300

    feat: add searchProductsByBrand function

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,21 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductsByBrand = (brand) => {
+  let newInfos = [];
+  if (!brand) {
+    return newInfos;
+  }
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (brand === stockProducts[index].brand) {
+      let description = stockProducts[index].description;
+      let price = stockProducts[index].price;
+      let formattedPrice = `R$ ${price}`;
+      newInfos.push({ description, formattedPrice });
+    }
+  }
+  return newInfos;
 };
+
+searchProductsByBrand();
 
 module.exports = { searchProductsByBrand };
