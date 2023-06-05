d50c8569c775fd1329d1ec86dd907b539eb0353e
Maires Souza <trabalho.maires@gmail.com>
2023-06-05 19:13:27 -0300

    feat: add getProductsOnSale function

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,22 @@
 const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+  let newInfos = [];
+  // if (!brand) {
+  //   return newInfos;
+  // }
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].onSale) {
+      let description = stockProducts[index].description;
+      let price = stockProducts[index].price;
+      let onSale = stockProducts[index].onSale;
+      let formattedPrice = `R$ ${price}`;
+      newInfos.push({ description, formattedPrice, onSale });
+    }
+  }
+  return newInfos;
 };
+
+getProductsOnSale();
 
 module.exports = { getProductsOnSale };
