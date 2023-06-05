0c494ede7e97f1ba5d1f598cb736be36fcfd29d5
Maires Souza <trabalho.maires@gmail.com>
2023-06-05 06:09:09 -0300

    feat: add getProductsAmount function

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -1,7 +1,15 @@
 const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
-  // Desenvolva seu código dentro dessa função...
+  let stockQuantity = 0;
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock > 0) {
+      stockQuantity += stockProducts[index].quantityInStock;
+    }
+  }
+  return stockQuantity;
 };
+
+console.log(getProductsAmount());
 
 module.exports = { getProductsAmount };
