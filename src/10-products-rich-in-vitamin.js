3a627a0dbb80a655dac17e8907a745c1ec86d1fe
Maires Souza <trabalho.maires@gmail.com>
2023-06-06 07:01:46 -0300

    feat: add getProductsRichInVitamin function

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,28 @@
 const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
+  let newInfos = [];
+  let vitInfos = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].nutritionalInfo.vitamins) {
+      let description = stockProducts[index].description;
+      let price = stockProducts[index].price;
+      let formattedPrice = `R$ ${price}`;
+      let vitamArray = stockProducts[index].nutritionalInfo.vitamins;
+      vitInfos.push(stockProducts[index].nutritionalInfo.vitamins);
+      let vitaminsInformation = [];
+      for ( let vitamin in vitamArray) {
+        if (Object.prototype.hasOwnProperty.call(vitamArray, vitamin)) {
+          let value = vitamArray[vitamin];
+        vitaminsInformation.push(`${vitamin} - ${value}`)
+        }
+      }
+      newInfos.push({ description, formattedPrice, vitaminsInformation });
+    }
+  }
+  return newInfos;
 };
+
+console.log(getProductsRichInVitamin());
 
 module.exports = { getProductsRichInVitamin };
