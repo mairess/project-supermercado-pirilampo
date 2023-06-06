b820971ae9a6c7364f618f5bf4da49626cc9f9e9
Maires Souza <trabalho.maires@gmail.com>
2023-06-06 07:21:02 -0300

    fix: eslint issues

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -2,19 +2,17 @@ const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
   let newInfos = [];
-  let vitInfos = [];
   for (let index = 0; index < stockProducts.length; index += 1) {
     if (stockProducts[index].nutritionalInfo.vitamins) {
       let description = stockProducts[index].description;
       let price = stockProducts[index].price;
       let formattedPrice = `R$ ${price}`;
       let vitamArray = stockProducts[index].nutritionalInfo.vitamins;
-      vitInfos.push(stockProducts[index].nutritionalInfo.vitamins);
       let vitaminsInformation = [];
-      for ( let vitamin in vitamArray) {
+      for (let vitamin in vitamArray) {
         if (Object.prototype.hasOwnProperty.call(vitamArray, vitamin)) {
           let value = vitamArray[vitamin];
-        vitaminsInformation.push(`${vitamin} - ${value}`)
+          vitaminsInformation.push(`${vitamin} - ${value}`);
         }
       }
       newInfos.push({ description, formattedPrice, vitaminsInformation });
